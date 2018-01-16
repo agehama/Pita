b7246c15788d728bc57cbe6e1da8c04e33fd8e51
agehama <agehama1@gmail.com>
2018-01-16 16:23:54 +0900

    refactor(cut out Vectorizer.hpp, Parser.hpp)

---
+++ Parser.hpp
@@ -0,0 +1,336 @@
+#pragma once
+#define BOOST_RESULT_OF_USE_DECLTYPE
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+#include "Node.hpp"
+
+namespace cgl
+{
+	inline auto MakeUnaryExpr(UnaryOp op)
+	{
+		return boost::phoenix::bind([](const auto & e, UnaryOp op) {return UnaryExpr(e, op); }, boost::spirit::_1, op);
+	}
+
+	inline auto MakeBinaryExpr(BinaryOp op)
+	{
+		return boost::phoenix::bind([&](const auto& lhs, const auto& rhs, BinaryOp op) {return BinaryExpr(lhs, rhs, op); }, boost::spirit::_val, boost::spirit::_1, op);
+	}
+
+	template <class F, class... Args>
+	inline auto Call(F func, Args... args)
+	{
+		return boost::phoenix::bind(func, args...);
+	}
+
+	template<class FromT, class ToT>
+	inline auto Cast()
+	{
+		return boost::phoenix::bind([&](const FromT& a) {return static_cast<ToT>(a); }, boost::spirit::_1);
+	}
+
+	using namespace boost::spirit;
+
+	template<typename Iterator>
+	struct SpaceSkipper : public qi::grammar<Iterator>
+	{
+		qi::rule<Iterator> skip;
+
+		SpaceSkipper() :SpaceSkipper::base_type(skip)
+		{
+			skip = +(lit(' ') ^ lit('\r') ^ lit('\t'));
+		}
+	};
+
+	template<typename Iterator>
+	struct LineSkipper : public qi::grammar<Iterator>
+	{
+		qi::rule<Iterator> skip;
+
+		LineSkipper() :LineSkipper::base_type(skip)
+		{
+			skip = ascii::space;
+		}
+	};
+
+	struct keywords_t : qi::symbols<char, qi::unused_type> {
+		keywords_t() {
+			add("for", qi::unused)
+				("in", qi::unused)
+				("sat", qi::unused)
+				("free", qi::unused);
+		}
+	} const keywords;
+
+	using IteratorT = std::string::const_iterator;
+	using SpaceSkipperT = SpaceSkipper<IteratorT>;
+	using LineSkipperT = LineSkipper<IteratorT>;
+
+	static SpaceSkipperT spaceSkipper;
+	static LineSkipperT lineSkipper;
+
+	using qi::char_;
+
+	template<typename Iterator, typename Skipper>
+	struct Parser
+		: qi::grammar<Iterator, Lines(), Skipper>
+	{
+		qi::rule<Iterator, DeclSat(), Skipper> constraints;
+		qi::rule<Iterator, DeclFree(), Skipper> freeVals;
+
+		qi::rule<Iterator, FunctionAccess(), Skipper> functionAccess;
+		qi::rule<Iterator, RecordAccess(), Skipper> recordAccess;
+		qi::rule<Iterator, ListAccess(), Skipper> listAccess;
+		qi::rule<Iterator, Accessor(), Skipper> accessor;
+
+		qi::rule<Iterator, Access(), Skipper> access;
+
+		qi::rule<Iterator, KeyExpr(), Skipper> record_keyexpr;
+		qi::rule<Iterator, RecordConstractor(), Skipper> record_maker;
+		qi::rule<Iterator, RecordInheritor(), Skipper> record_inheritor;
+
+		qi::rule<Iterator, ListConstractor(), Skipper> list_maker;
+		qi::rule<Iterator, For(), Skipper> for_expr;
+		qi::rule<Iterator, If(), Skipper> if_expr;
+		qi::rule<Iterator, Return(), Skipper> return_expr;
+		qi::rule<Iterator, DefFunc(), Skipper> def_func;
+		qi::rule<Iterator, Arguments(), Skipper> arguments;
+		qi::rule<Iterator, Identifier(), Skipper> id;
+		qi::rule<Iterator, Expr(), Skipper> general_expr, logic_expr, logic_term, logic_factor, compare_expr, arith_expr, basic_arith_expr, term, factor, pow_term, pow_term1;
+		qi::rule<Iterator, Lines(), Skipper> expr_seq, statement;
+		qi::rule<Iterator, Lines(), Skipper> program;
+
+		//qi::rule<Iterator, std::string(), Skipper> double_value;
+		//qi::rule<Iterator, Identifier(), Skipper> double_value, double_value2;
+
+		qi::rule<Iterator> s, s1;
+		qi::rule<Iterator> distinct_keyword;
+		qi::rule<Iterator, std::string(), Skipper> unchecked_identifier;
+		
+		Parser() : Parser::base_type(program)
+		{
+			auto concatLines = [](Lines& lines, Expr& expr) { lines.concat(expr); };
+
+			auto makeLines = [](Expr& expr) { return Lines(expr); };
+
+			auto makeDefFunc = [](const Arguments& arguments, const Expr& expr) { return DefFunc(arguments, expr); };
+
+			//auto addCharacter = [](Identifier& identifier, char c) { identifier.name.push_back(c); };
+
+			auto concatArguments = [](Arguments& a, const Arguments& b) { a.concat(b); };
+
+			auto applyFuncDef = [](DefFunc& f, const Expr& expr) { f.expr = expr; };
+
+			//auto makeDouble = [](const Identifier& str) { return std::stod(str.name); };
+			//auto makeString = [](char c) { return Identifier(std::string({ c })); };
+			//auto appendString = [](Identifier& str, char c) { str.name.push_back(c); };
+			//auto appendString2 = [](Identifier& str, const Identifier& str2) { str.name.append(str2.name); };
+
+			program = s >> -(expr_seq) >> s;
+
+			/*expr_seq = general_expr[_val = Call(makeLines, _1)] >> *(
+				(s >> ',' >> s >> general_expr[Call(concatLines, _val, _1)])
+				| (+(lit('\n')) >> general_expr[Call(concatLines, _val, _1)])
+				);*/
+
+			expr_seq = statement[_val = _1] >> *(
+				+(lit('\n')) >> statement[Call(Lines::Concat, _val, _1)]
+				);
+
+			statement = general_expr[_val = Call(Lines::Make, _1)] >> *(
+				(s >> ',' >> s >> general_expr[Call(Lines::Append, _val, _1)])
+				| (lit('\n') >> general_expr[Call(Lines::Append, _val, _1)])
+				);
+
+			general_expr =
+				if_expr[_val = _1]
+				| return_expr[_val = _1]
+				| logic_expr[_val = _1];
+
+			if_expr = lit("if") >> s >> general_expr[_val = Call(If::Make, _1)]
+				>> s >> lit("then") >> s >> general_expr[Call(If::SetThen, _val, _1)]
+				>> -(s >> lit("else") >> s >> general_expr[Call(If::SetElse, _val, _1)])
+				;
+
+			for_expr = lit("for") >> s >> id[_val = Call(For::Make, _1)] >> s >> lit("in")
+				>> s >> general_expr[Call(For::SetRangeStart, _val, _1)] >> s >> lit(":")
+				>> s >> general_expr[Call(For::SetRangeEnd, _val, _1)] >> s >> lit("do")
+				>> s >> general_expr[Call(For::SetDo, _val, _1)];
+
+			return_expr = lit("return") >> s >> general_expr[_val = Call(Return::Make, _1)];
+
+			def_func = arguments[_val = _1] >> lit("->") >> s >> statement[Call(applyFuncDef, _val, _1)];
+			//def_func = arguments[_val = _1] >> lit("->") >> s >> general_expr[Call(applyFuncDef, _val, _1)];
+
+			//constraintはDNFの形で与えられるものとする
+			constraints = lit("sat") >> '(' >> s >> logic_expr[_val = Call(DeclSat::Make, _1)] >> s >> ')';
+
+			//freeValsがレコードへの参照とかを入れるのは少し大変だが、単一の値への参照なら難しくないはず
+			/*freeVals = lit("free") >> '(' >> s >> (accessor[Call(DeclFree::AddAccessor, _val, _1)] | id[Call(DeclFree::AddIdentifier, _val, _1)]) >> *(
+				s >> ", " >> s >> (accessor[Call(DeclFree::AddAccessor, _val, _1)] | id[Call(DeclFree::AddIdentifier, _val, _1)])
+				) >> s >> ')';*/
+			/*freeVals = lit("free") >> '(' >> s >> (accessor[Call(DeclFree::AddAccessor, _val, _1)] | id[Call(DeclFree::AddAccessor, _val, _1)]) >> *(
+				s >> ", " >> s >> (accessor[Call(DeclFree::AddAccessor, _val, _1)] | id[Call(DeclFree::AddAccessor, _val, _1)])
+				) >> s >> ')';*/
+
+			freeVals = lit("free") >> '(' >> s >> (accessor[Call(DeclFree::AddAccessor, _val, _1)] | id[Call(DeclFree::AddAccessor, _val, Cast<Identifier, Accessor>())]) >> *(
+				s >> ", " >> s >> (accessor[Call(DeclFree::AddAccessor, _val, _1)] | id[Call(DeclFree::AddAccessor, _val, Cast<Identifier, Accessor>())])
+				) >> s >> ')';
+
+			/*freeVals = lit("free") >> '(' >> s >> id[Call(DeclFree::AddIdentifier, _val, _1)] >> *(
+				s >> "," >> s >> id[Call(DeclFree::AddIdentifier, _val, _1)]
+				) >> s >> ')';*/
+
+			arguments = -(id[_val = _1] >> *(s >> ',' >> s >> arguments[Call(concatArguments, _val, _1)]));
+
+			logic_expr = logic_term[_val = _1] >> *(s >> '|' >> s >> logic_term[_val = MakeBinaryExpr(BinaryOp::Or)]);
+
+			logic_term = logic_factor[_val = _1] >> *(s >> '&' >> s >> logic_factor[_val = MakeBinaryExpr(BinaryOp::And)]);
+
+			logic_factor = ('!' >> s >> compare_expr[_val = MakeUnaryExpr(UnaryOp::Not)])
+				| compare_expr[_val = _1]
+				;
+
+			compare_expr = arith_expr[_val = _1] >> *(
+				(s >> lit("==") >> s >> arith_expr[_val = MakeBinaryExpr(BinaryOp::Equal)])
+				| (s >> lit("!=") >> s >> arith_expr[_val = MakeBinaryExpr(BinaryOp::NotEqual)])
+				| (s >> lit("<") >> s >> arith_expr[_val = MakeBinaryExpr(BinaryOp::LessThan)])
+				| (s >> lit("<=") >> s >> arith_expr[_val = MakeBinaryExpr(BinaryOp::LessEqual)])
+				| (s >> lit(">") >> s >> arith_expr[_val = MakeBinaryExpr(BinaryOp::GreaterThan)])
+				| (s >> lit(">=") >> s >> arith_expr[_val = MakeBinaryExpr(BinaryOp::GreaterEqual)])
+				)
+				;
+
+			//= ^ -> は右結合
+
+			arith_expr = (basic_arith_expr[_val = _1] >> -(s >> '=' >> s >> arith_expr[_val = MakeBinaryExpr(BinaryOp::Assign)]));
+
+			basic_arith_expr = term[_val = _1] >>
+				*((s >> '+' >> s >> term[_val = MakeBinaryExpr(BinaryOp::Add)]) |
+				(s >> '-' >> s >> term[_val = MakeBinaryExpr(BinaryOp::Sub)]))
+				;
+
+			term = pow_term[_val = _1]
+				| (factor[_val = _1] >>
+					*((s >> '*' >> s >> pow_term1[_val = MakeBinaryExpr(BinaryOp::Mul)]) |
+					(s >> '/' >> s >> pow_term1[_val = MakeBinaryExpr(BinaryOp::Div)]))
+					)
+				;
+			
+			//最低でも1つは受け取るようにしないと、単一のfactorを受理できてしまうのでMul,Divの方に行ってくれない
+			pow_term = factor[_val = _1] >> s >> '^' >> s >> pow_term1[_val = MakeBinaryExpr(BinaryOp::Pow)];
+			pow_term1 = factor[_val = _1] >> -(s >> '^' >> s >> pow_term1[_val = MakeBinaryExpr(BinaryOp::Pow)]);
+
+			//record{} の間には改行は挟めない（record,{}と区別できなくなるので）
+			record_inheritor = id[_val = Call(RecordInheritor::Make, _1)] >> record_maker[Call(RecordInheritor::AppendRecord, _val, _1)];
+
+			record_maker = (
+				char_('{') >> s >> (record_keyexpr[Call(RecordConstractor::AppendKeyExpr, _val, _1)] | general_expr[Call(RecordConstractor::AppendExpr, _val, _1)]) >>
+				*(
+				(s >> ',' >> s >> (record_keyexpr[Call(RecordConstractor::AppendKeyExpr, _val, _1)] | general_expr[Call(RecordConstractor::AppendExpr, _val, _1)]))
+					| (+(char_('\n')) >> (record_keyexpr[Call(RecordConstractor::AppendKeyExpr, _val, _1)] | general_expr[Call(RecordConstractor::AppendExpr, _val, _1)]))
+					)
+				>> s >> char_('}')
+				)
+				| (char_('{') >> s >> char_('}'));
+
+			//レコードの name:val の name と : の間に改行を許すべきか？ -> 許しても解析上恐らく問題はないが、意味があまりなさそう
+			record_keyexpr = id[_val = Call(KeyExpr::Make, _1)] >> char_(':') >> s >> general_expr[Call(KeyExpr::SetExpr, _val, _1)];
+
+			/*list_maker = (char_('[') >> s >> general_expr[_val = Call(ListConstractor::Make, _1)] >>
+				*(s >> char_(',') >> s >> general_expr[Call(ListConstractor::Append, _val, _1)]) >> s >> char_(']')
+				)
+				| (char_('[') >> s >> char_(']'));*/
+
+			list_maker = (char_('[') >> s >> general_expr[_val = Call(ListConstractor::Make, _1)] >>
+				*(
+					(s >> char_(',') >> s >> general_expr[Call(ListConstractor::Append, _val, _1)])
+					| (+(char_('\n')) >> general_expr[Call(ListConstractor::Append, _val, _1)])
+					) >> s >> char_(']')
+				)
+				| (char_('[') >> s >> char_(']'));
+			
+			accessor = (id[_val = Call(Accessor::Make, _1)] >> +(access[Call(Accessor::Append, _val, _1)]))
+				| (list_maker[_val = Call(Accessor::Make, _1)] >> listAccess[Call(Accessor::AppendList, _val, _1)] >> *(access[Call(Accessor::Append, _val, _1)]))
+				| (record_maker[_val = Call(Accessor::Make, _1)] >> recordAccess[Call(Accessor::AppendRecord, _val, _1)] >> *(access[Call(Accessor::Append, _val, _1)]));
+			
+			access = functionAccess[_val = Cast<FunctionAccess, Access>()]
+				| listAccess[_val = Cast<ListAccess, Access>()]
+				| recordAccess[_val = Cast<RecordAccess, Access>()];
+
+			recordAccess = char_('.') >> s >> id[_val = Call(RecordAccess::Make, _1)];
+
+			listAccess = char_('[') >> s >> general_expr[Call(ListAccess::SetIndex, _val, _1)] >> s >> char_(']');
+
+			functionAccess = char_('(')
+				>> -(s >> general_expr[Call(FunctionAccess::Append, _val, _1)])
+				>> *(s >> char_(',') >> s >> general_expr[Call(FunctionAccess::Append, _val, _1)]) >> s >> char_(')');
+
+			//factor = /*double_[_val = _1]
+			//	| */int_[_val = _1]
+			//	| lit("true")[_val = true]
+			//	| lit("false")[_val = false]
+			//	| '(' >> s >> expr_seq[_val = _1] >> s >> ')'
+			//	| '+' >> s >> factor[_val = MakeUnaryExpr(UnaryOp::Plus)]
+			//	| '-' >> s >> factor[_val = MakeUnaryExpr(UnaryOp::Minus)]
+			//	| constraints[_val = _1]
+			//	| freeVals[_val = _1]
+			//	| accessor[_val = _1]
+			//	| def_func[_val = _1]
+			//	| for_expr[_val = _1]
+			//	| list_maker[_val = _1]
+			//	| record_inheritor[_val = _1]
+			//	//| (id >> record_maker)[_val = Call(RecordInheritor::MakeRecord, _1,_2)]
+			//	| record_maker[_val = _1]
+			//	| id[_val = _1];
+			
+			factor = /*double_[_val = _1]
+					 | */int_[_val = Call(LRValue::Int, _1)]
+				| lit("true")[_val = Call(LRValue::Bool, true)]
+				| lit("false")[_val = Call(LRValue::Bool, false)]
+				| '(' >> s >> expr_seq[_val = _1] >> s >> ')'
+				| '+' >> s >> factor[_val = MakeUnaryExpr(UnaryOp::Plus)]
+				| '-' >> s >> factor[_val = MakeUnaryExpr(UnaryOp::Minus)]
+				//| constraints[_val = Call(LRValue::Sat, _1)]
+				| constraints[_val = _1]
+				| freeVals[_val = Call(LRValue::Free, _1)]
+				| accessor[_val = _1]
+				| def_func[_val = _1]
+				| for_expr[_val = _1]
+				| list_maker[_val = _1]
+				| record_inheritor[_val = _1]
+				//| (id >> record_maker)[_val = Call(RecordInheritor::MakeRecord, _1,_2)]
+				| record_maker[_val = _1]
+				| id[_val = _1];
+
+			//idの途中には空白を含めない
+			//id = lexeme[ascii::alpha[_val = _1] >> *(ascii::alnum[Call(addCharacter, _val, _1)])];
+			//id = identifier_def[_val = _1];
+			id = unchecked_identifier[_val = _1] - distinct_keyword;
+
+			distinct_keyword = qi::lexeme[keywords >> !(qi::alnum | '_')];
+			unchecked_identifier = qi::lexeme[(qi::alpha | qi::char_('_')) >> *(qi::alnum | qi::char_('_'))];
+
+			/*auto const distinct_keyword = qi::lexeme[keywords >> !(qi::alnum | '_')];
+			auto const unchecked_identifier = qi::lexeme[(qi::alpha | qi::char_('_')) >> *(qi::alnum | qi::char_('_'))];
+			auto const identifier_def = unchecked_identifier - distinct_keyword;*/
+
+			s = *(ascii::space);
+			/*s = -(ascii::space) >> -(s1);
+			s1 = ascii::space >> -(s1);*/
+
+			//double_ だと 1. とかもパースできてしまうせいでレンジのパースに支障が出るので別に定義する
+			//double_value = lexeme[qi::char_('1', '9') >> *(ascii::digit) >> lit(".") >> +(ascii::digit)  [_val = Call(makeDouble, _1)]];
+			/*double_value = lexeme[qi::char_('1', '9')[_val = Call(makeString, _1)] >> *(ascii::digit[Call(appendString, _val, _1)])
+				>> lit(".")[Call(appendString, _val, _1)] >> +(ascii::digit[Call(appendString, _val, _1)])];*/
+			/*double_value = lexeme[qi::char_('1', '9')[_val = _1] >> *(ascii::digit[Call(appendString, _val, _1)])
+				>> lit(".")[Call(appendString, _val, _1)] >> +(ascii::digit[Call(appendString, _val, _1)])];*/
+			
+			/*double_value = lexeme[qi::char_('1', '9')[_val = _1] >> *(ascii::digit[Call(appendString, _val, _1)]) >> double_value2[Call(appendString2, _val, _1)]];
+
+			double_value2 = lexeme[lit(".")[_val = '.'] >> +(ascii::digit[Call(appendString, _val, _1)])];*/
+		}
+	};
+}
---
+++ Vectorizer.hpp
@@ -0,0 +1,480 @@
+#pragma once
+#include <Eigen/Core>
+
+#include "Node.hpp"
+#include "Printer.hpp"
+#include "Environment.hpp"
+#include "Evaluator.hpp"
+#include "OptimizationEvaluator.hpp"
+#include "Parser.hpp"
+
+extern std::ofstream ofs;
+
+namespace cgl
+{
+	inline bool ReadDouble(double& output, const std::string& name, const Record& record, std::shared_ptr<Environment> environment)
+	{
+		const auto& values = record.values;
+		auto it = values.find(name);
+		if (it == values.end())
+		{
+			//CGL_DebugLog(__FUNCTION__);
+			return false;
+		}
+		auto opt = environment->expandOpt(it->second);
+		if (!opt)
+		{
+			//CGL_DebugLog(__FUNCTION__);
+			return false;
+		}
+		const Evaluated& value = opt.value();
+		if (!IsType<int>(value) && !IsType<double>(value))
+		{
+			//CGL_DebugLog(__FUNCTION__);
+			return false;
+		}
+		output = IsType<int>(value) ? static_cast<double>(As<int>(value)) : As<double>(value);
+		return true;
+	}
+
+	struct Transform
+	{
+		Transform()
+		{
+			init();
+		}
+
+		Transform(const Eigen::Matrix3d& mat) :mat(mat) {}
+
+		Transform(const Record& record, std::shared_ptr<Environment> pEnv)
+		{
+			double px = 0, py = 0;
+			double sx = 1, sy = 1;
+			double angle = 0;
+
+			for (const auto& member : record.values)
+			{
+				auto valOpt = AsOpt<Record>(pEnv->expand(member.second));
+
+				if (member.first == "pos" && valOpt)
+				{
+					ReadDouble(px, "x", valOpt.value(), pEnv);
+					ReadDouble(py, "y", valOpt.value(), pEnv);
+				}
+				else if (member.first == "scale" && valOpt)
+				{
+					ReadDouble(sx, "x", valOpt.value(), pEnv);
+					ReadDouble(sy, "y", valOpt.value(), pEnv);
+				}
+				else if (member.first == "angle")
+				{
+					ReadDouble(angle, "angle", record, pEnv);
+				}
+			}
+
+			init(px, py, sx, sy, angle);
+		}
+
+		void init(double px = 0, double py = 0, double sx = 1, double sy = 1, double angle = 0)
+		{
+			const double pi = 3.1415926535;
+			const double cosTheta = std::cos(pi*angle/180.0);
+			const double sinTheta = std::sin(pi*angle/180.0);
+
+			mat <<
+				sx*cosTheta, -sy*sinTheta, px,
+				sx*sinTheta, sy*cosTheta, py,
+				0, 0, 1;
+		}
+
+		Transform operator*(const Transform& other)const
+		{
+			return mat * other.mat;
+		}
+
+		Eigen::Vector2d product(const Eigen::Vector2d& v)const
+		{
+			Eigen::Vector3d xs;
+			xs << v.x(), v.y(), 1;
+			Eigen::Vector3d result = mat * xs;
+			Eigen::Vector2d result2d;
+			result2d << result.x(), result.y();
+			return result2d;
+		}
+
+		void printMat()const
+		{
+			std::cout << "Matrix(\n";
+			for (int y = 0; y < 3; ++y)
+			{
+				std::cout << "    ";
+				for (int x = 0; x < 3; ++x)
+				{
+					std::cout << mat(y, x) << " ";
+				}
+				std::cout << "\n";
+			}
+			std::cout << ")\n";
+		}
+
+	private:
+		Eigen::Matrix3d mat;
+	};
+
+	template<class T>
+	using Vector = std::vector<T, Eigen::aligned_allocator<T>>;
+
+	inline bool ReadPolygon(Vector<Eigen::Vector2d>& output, const List& vertices, std::shared_ptr<Environment> pEnv, const Transform& transform)
+	{
+		output.clear();
+
+		for (const Address vertex : vertices.data)
+		{
+			//CGL_DebugLog(__FUNCTION__);
+			const Evaluated value = pEnv->expand(vertex);
+
+			//CGL_DebugLog(__FUNCTION__);
+			if (IsType<Record>(value))
+			{
+				double x = 0, y = 0;
+				const Record& pos = As<Record>(value);
+				//CGL_DebugLog(__FUNCTION__);
+				if (!ReadDouble(x, "x", pos, pEnv) || !ReadDouble(y, "y", pos, pEnv))
+				{
+					//CGL_DebugLog(__FUNCTION__);
+					return false;
+				}
+				//CGL_DebugLog(__FUNCTION__);
+				Eigen::Vector2d v;
+				v << x, y;
+				//CGL_DebugLog(ToS(v.x()) + ", " + ToS(v.y()));
+				output.push_back(transform.product(v));
+				//CGL_DebugLog(__FUNCTION__);
+			}
+			else
+			{
+				//CGL_DebugLog(__FUNCTION__);
+				return false;
+			}
+		}
+
+		//CGL_DebugLog(__FUNCTION__);
+		return true;
+	}
+
+	inline void OutputShapeList(std::ostream& os, const List& list, std::shared_ptr<Environment> pEnv, const Transform& transform);
+
+	inline void OutputSVGImpl(std::ostream& os, const Record& record, std::shared_ptr<Environment> pEnv, const Transform& parent = Transform())
+	{
+		const Transform current(record, pEnv);
+
+		const Transform transform = parent * current;
+
+		for (const auto& member : record.values)
+		{
+			const Evaluated value = pEnv->expand(member.second);
+
+			if (member.first == "vertex" && IsType<List>(value))
+			{
+				Vector<Eigen::Vector2d> polygon;
+				if (ReadPolygon(polygon, As<List>(value), pEnv, transform) && !polygon.empty())
+				{
+					//CGL_DebugLog(__FUNCTION__);
+
+					os << "<polygon points=\"";
+					for (const auto& vertex : polygon)
+					{
+						os << vertex.x() << "," << vertex.y() << " ";
+					}
+					os << "\"/>\n";
+				}
+				else
+				{
+					//CGL_DebugLog(__FUNCTION__);
+				}
+					
+			}
+			else if (IsType<Record>(value))
+			{
+				OutputSVGImpl(os, As<Record>(value), pEnv, transform);
+			}
+			else if (IsType<List>(value))
+			{
+				OutputShapeList(os, As<List>(value), pEnv, transform);
+			}
+		}
+	}
+
+	inline void OutputShapeList(std::ostream& os, const List& list, std::shared_ptr<Environment> pEnv, const Transform& transform)
+	{
+		for (const Address member : list.data)
+		{
+			const Evaluated value = pEnv->expand(member);
+
+			if (IsType<Record>(value))
+			{
+				OutputSVGImpl(os, As<Record>(value), pEnv, transform);
+			}
+			else if (IsType<List>(value))
+			{
+				OutputShapeList(os, As<List>(value), pEnv, transform);
+			}
+		}
+	}
+
+	class BoundingRect
+	{
+	public:
+		void add(const Eigen::Vector2d& v)
+		{
+			if (v.x() < m_min.x())
+			{
+				m_min.x() = v.x();
+			}
+			if (v.y() < m_min.y())
+			{
+				m_min.y() = v.y();
+			}
+			if (m_max.x() < v.x())
+			{
+				m_max.x() = v.x();
+			}
+			if (m_max.y() < v.y())
+			{
+				m_max.y() = v.y();
+			}
+		}
+
+		bool intersects(const BoundingRect& other)const
+		{
+			return std::max(m_min.x(), other.m_min.x()) < std::min(m_max.x(), other.m_max.x())
+				&& std::max(m_min.y(), other.m_min.y()) < std::min(m_max.y(), other.m_max.y());
+		}
+
+		bool includes(const Eigen::Vector2d& point)const
+		{
+			return m_min.x() < point.x() && point.x() < m_max.x()
+				&& m_min.y() < point.y() && point.y() < m_max.y();
+		}
+
+		Eigen::Vector2d pos()const
+		{
+			return m_min;
+		}
+
+		Eigen::Vector2d center()const
+		{
+			return (m_min + m_max)*0.5;
+		}
+				
+		Eigen::Vector2d width()const
+		{
+			return m_max - m_min;
+		}
+
+	private:
+		Eigen::Vector2d m_min = Eigen::Vector2d(DBL_MAX, DBL_MAX);
+		Eigen::Vector2d m_max = Eigen::Vector2d(-DBL_MAX, -DBL_MAX);
+	};
+
+	inline void GetBoundingBoxImpl(BoundingRect& output, const List& list, std::shared_ptr<Environment> pEnv, const Transform& transform);
+
+	inline void GetBoundingBoxImpl(BoundingRect& output, const Record& record, std::shared_ptr<Environment> pEnv, const Transform& parent = Transform())
+	{
+		const Transform current(record, pEnv);
+		const Transform transform = parent * current;
+
+		for (const auto& member : record.values)
+		{
+			const Evaluated value = pEnv->expand(member.second);
+
+			if (member.first == "vertex" && IsType<List>(value))
+			{
+				Vector<Eigen::Vector2d> polygon;
+				if (ReadPolygon(polygon, As<List>(value), pEnv, transform) && !polygon.empty())
+				{
+					for (const auto& vertex : polygon)
+					{
+						output.add(vertex);
+					}
+				}
+
+			}
+			else if (IsType<Record>(value))
+			{
+				GetBoundingBoxImpl(output, As<Record>(value), pEnv, transform);
+			}
+			else if (IsType<List>(value))
+			{
+				GetBoundingBoxImpl(output, As<List>(value), pEnv, transform);
+			}
+		}
+	}
+
+	inline void GetBoundingBoxImpl(BoundingRect& output, const List& list, std::shared_ptr<Environment> pEnv, const Transform& transform)
+	{
+		for (const Address member : list.data)
+		{
+			const Evaluated value = pEnv->expand(member);
+
+			if (IsType<Record>(value))
+			{
+				GetBoundingBoxImpl(output, As<Record>(value), pEnv, transform);
+			}
+			else if (IsType<List>(value))
+			{
+				GetBoundingBoxImpl(output, As<List>(value), pEnv, transform);
+			}
+		}
+	}
+
+	inline boost::optional<BoundingRect> GetBoundingBox(const Evaluated& value, std::shared_ptr<Environment> pEnv)
+	{
+		if (IsType<Record>(value))
+		{
+			const Record& record = As<Record>(value);
+			BoundingRect rect;
+			GetBoundingBoxImpl(rect, record, pEnv);
+			return rect;
+		}
+
+		return boost::none;
+	}
+
+	inline bool OutputSVG(std::ostream& os, const Evaluated& value, std::shared_ptr<Environment> pEnv)
+	{
+		auto boundingBoxOpt = GetBoundingBox(value, pEnv);
+		if (IsType<Record>(value) && boundingBoxOpt)
+		{
+			const BoundingRect& rect = boundingBoxOpt.value();
+
+			//const auto pos = rect.pos();
+			const auto widthXY = rect.width();
+			const auto center = rect.center();
+
+			const double width = std::max(widthXY.x(), widthXY.y());
+			const double halfWidth = width*0.5;
+
+			const Eigen::Vector2d pos = center - Eigen::Vector2d(halfWidth, halfWidth);
+
+			//os << R"(<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">)" << "\n";
+			os << R"(<svg xmlns="http://www.w3.org/2000/svg" width=")" << width << R"(" height=")" << width << R"(" viewBox=")" << pos.x() << " " << pos.y() << " " << width << " " << width << R"(">)" << "\n";
+
+			const Record& record = As<Record>(value);
+			OutputSVGImpl(os, record, pEnv);
+
+			os << "</svg>" << "\n";
+
+			return true;
+		}
+
+		return false;
+	}
+
+	class Program
+	{
+	public:
+		Program() :
+			pEnv(Environment::Make()),
+			evaluator(pEnv)
+		{}
+
+		boost::optional<Expr> parse(const std::string& program)
+		{
+			using namespace cgl;
+
+			Lines lines;
+
+			SpaceSkipper<IteratorT> skipper;
+			Parser<IteratorT, SpaceSkipperT> grammer;
+
+			std::string::const_iterator it = program.begin();
+			if (!boost::spirit::qi::phrase_parse(it, program.end(), grammer, skipper, lines))
+			{
+				std::cerr << "Syntax Error: parse failed\n";
+				return boost::none;
+			}
+
+			if (it != program.end())
+			{
+				std::cerr << "Syntax Error: ramains input\n" << std::string(it, program.end());
+				return boost::none;
+			}
+
+			return lines;
+		}
+
+		boost::optional<Evaluated> execute(const std::string& program)
+		{
+			if (auto exprOpt = parse(program))
+			{
+				try
+				{
+					return pEnv->expand(boost::apply_visitor(evaluator, exprOpt.value()));
+				}
+				catch (const cgl::Exception& e)
+				{
+					std::cerr << "Exception: " << e.what() << std::endl;
+				}
+			}
+
+			return boost::none;
+		}
+
+		bool draw(const std::string& program)
+		{
+			std::cout << "parse..." << std::endl;
+			if (auto exprOpt = parse(program))
+			{
+				try
+				{
+					std::cout << "parse succeeded" << std::endl;
+					printExpr(exprOpt.value());
+
+					std::cout << "execute..." << std::endl;
+					const LRValue lrvalue = boost::apply_visitor(evaluator, exprOpt.value());
+					const Evaluated result = pEnv->expand(lrvalue);
+					std::cout << "execute succeeded" << std::endl;
+
+					std::cout << "output SVG..." << std::endl;
+					OutputSVG(std::cout, result, pEnv);
+					std::cout << "output succeeded" << std::endl;
+				}
+				catch (const cgl::Exception& e)
+				{
+					std::cerr << "Exception: " << e.what() << std::endl;
+				}
+			}
+
+			return false;
+		}
+
+		void clear()
+		{
+			pEnv = Environment::Make();
+			evaluator = Eval(pEnv);
+		}
+
+		bool test(const std::string& program, const Expr& expr)
+		{
+			clear();
+
+			if (auto result = execute(program))
+			{
+				std::shared_ptr<Environment> pEnv2 = Environment::Make();
+				Eval evaluator2(pEnv2);
+
+				const Evaluated answer = pEnv->expand(boost::apply_visitor(evaluator2, expr));
+
+				return IsEqualEvaluated(result.value(), answer);
+			}
+
+			return false;
+		}
+
+	private:
+
+		std::shared_ptr<Environment> pEnv;
+		Eval evaluator;
+	};
+}
